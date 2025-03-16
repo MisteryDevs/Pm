507580085ced5f77c55fc66a5a67be03cf064973
princesinff <166802071+princesinff@users.noreply.github.com>
2025-03-17 05:24:28 +0530

    Update help.py

--- SONALI/utils/inline/help.py
+++ SONALI/utils/inline/help.py
@@ -2,7 +2,7 @@ from typing import Union
 
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 
-from PRINCE import app
+from SONALI import app
 
 
 def help_pannel(_, START: Union[bool, int] = None, page: int = 1):
