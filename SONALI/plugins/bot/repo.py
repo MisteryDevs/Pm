a1ad5b61b9bfac73ae4153709ca8371a05847d1a
princesinff <166802071+princesinff@users.noreply.github.com>
2025-03-16 07:42:04 +0530

    Update repo.py

--- SONALI/plugins/bot/repo.py
+++ SONALI/plugins/bot/repo.py
@@ -1,6 +1,6 @@
 from pyrogram import filters
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
-from TanuMusic import app
+from SONALI import app
 from config import BOT_USERNAME
 
 start_txt = """
