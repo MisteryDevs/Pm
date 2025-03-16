ff060b3c4626484763da0bb23b11f89f84bfa70d
princesinff <166802071+princesinff@users.noreply.github.com>
2025-03-16 07:35:13 +0530

    Create repo.py

---
+++ SONALI/plugins/bot/repo.py
@@ -0,0 +1,28 @@
+from pyrogram import filters
+from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
+from TanuMusic import app
+from config import BOT_USERNAME
+
+start_txt = """
+❖ ʜᴇʏ , ᴛʜᴇʀᴇ ɴɪᴄᴇ ᴛᴏ ᴍᴇᴇᴛ ʏᴏᴜ  ♥︎\n\n● ɪғ ʏᴏᴜ ᴡᴀɴᴛ "•⏤‌𝄞⃝🍧 ‌⃪‌𝐒ᴡᴇᴇᴛʏ 𝐌ᴜsɪᴄ♥️꯭꯭꯭꯭ ꯭꯭᪳𝆺゙゙𝅥, ʙᴏᴛ ʀᴇᴘᴏ ᴛʜᴇɴ ᴄʟɪᴄᴋ ᴏɴ ᴛʜᴇ sᴏᴜʀᴄᴇ ᴄᴏᴅᴇ ʙᴜᴛᴛᴏɴ ᴛᴏ ᴄᴏʟʟᴇᴄᴛ ᴍʏ sᴏᴜʀᴄᴇ ᴄᴏᴅᴇ.\n\n❖ ᴘᴏᴡᴇʀᴇᴅ ʙʏ ➥ •⏤‌𝄞⃝🍧 ‌⃪‌𝐒ᴡᴇᴇᴛʏ 𝐌ᴜsɪᴄ♥️꯭꯭꯭꯭ ꯭꯭᪳𝆺𝅥 """
+
+
+
+
+@app.on_message(filters.command("repo"))
+async def start(_, msg):
+    buttons = [
+        [
+          InlineKeyboardButton("sᴜᴘᴘᴏʀᴛ", url="https://t.me/SWEETY_BOT_UPDATE"),
+          InlineKeyboardButton("ʀᴇᴘᴏ", url="https://t.me/MUSICCCCCCCCCCCC_PUR/200")
+          ],
+    ]
+    
+    reply_markup = InlineKeyboardMarkup(buttons)
+    
+    await msg.reply_photo(
+        photo="https://i.ibb.co/xSQPypBt/IMG-20250315-235146-523.jpg",
+        caption=start_txt,
+        reply_markup=reply_markup
+    )
+  
