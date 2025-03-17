20557af9292ed15a854b065fd1bdb22b517c4d38
princesinff <166802071+princesinff@users.noreply.github.com>
2025-03-17 20:36:41 +0530

    Update help.py

--- SONALI/utils/inline/help.py
+++ SONALI/utils/inline/help.py
@@ -1,33 +1,34 @@
 # Importing important modules & bot
+
 from SONALI import app
 from typing import Union
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 
-# Creating first partition of menu
+#------------------------------------------------------------------------#
+
+
+
+# creating first partition of menu
+
 def first_page(_):
-	controll_button = [
-	    InlineKeyboardButton(text="◁", callback_data="help_prev"), 
-	    InlineKeyboardButton(text="HOME", callback_data="settings_back_helper"), 
-	    InlineKeyboardButton(text="▷", callback_data="help_next")  # Fixed next button
-	]
+	controll_button = [InlineKeyboardButton(text="◁", callback_data=f"Adisa"), InlineKeyboardButton(text="HOME", callback_data=f"settingsback_helper"), InlineKeyboardButton(text="▷", callback_data=f"dilXaditi")]
 	first_page_menu = InlineKeyboardMarkup(
 		[
-			[InlineKeyboardButton(text=_["H_B_1"], callback_data="help_callback hb1"), InlineKeyboardButton(text=_["H_B_2"], callback_data="help_callback hb2"), InlineKeyboardButton(text=_["H_B_3"], callback_data="help_callback hb3")],
-			[InlineKeyboardButton(text=_["H_B_4"], callback_data="help_callback hb4"), InlineKeyboardButton(text=_["H_B_5"], callback_data="help_callback hb5"), InlineKeyboardButton(text=_["H_B_6"], callback_data="help_callback hb6")],
-			[InlineKeyboardButton(text=_["H_B_7"], callback_data="help_callback hb7"), InlineKeyboardButton(text=_["H_B_8"], callback_data="help_callback hb8"), InlineKeyboardButton(text=_["H_B_9"], callback_data="help_callback hb9")],
-			[InlineKeyboardButton(text=_["H_B_10"], callback_data="help_callback hb10"), InlineKeyboardButton(text=_["H_B_11"], callback_data="help_callback hb11"), InlineKeyboardButton(text=_["H_B_12"], callback_data="help_callback hb12")],
+			[InlineKeyboardButton(text=_["H_B_1"], callback_data="help_callback hb1"), InlineKeyboardButton(text=_["H_B_2"], callback_data="help_callback hb2"),InlineKeyboardButton(text=_["H_B_3"], callback_data="help_callback hb3")],
+			[InlineKeyboardButton(text=_["H_B_4"], callback_data="help_callback hb4"), InlineKeyboardButton(text=_["H_B_5"], callback_data="help_callback hb5"),InlineKeyboardButton(text=_["H_B_6"], callback_data="help_callback hb6")],
+			[InlineKeyboardButton(text=_["H_B_7"], callback_data="help_callback hb7"), InlineKeyboardButton(text=_["H_B_8"], callback_data="help_callback hb8"),InlineKeyboardButton(text=_["H_B_9"], callback_data="help_callback hb9")],
+			[InlineKeyboardButton(text=_["H_B_10"], callback_data="help_callback hb10"), InlineKeyboardButton(text=_["H_B_11"], callback_data="help_callback hb11"),InlineKeyboardButton(text=_["H_B_12"], callback_data="help_callback hb12")],
 			controll_button,
 		]
 	)
 	return first_page_menu
 
-# Creating second partition of menu
+
+
+# creating second partition of menu
+
 def second_page(_):
-	controll_button = [
-	    InlineKeyboardButton(text="◁", callback_data="help_prev"),  # Fixed previous button
-	    InlineKeyboardButton(text="HOME", callback_data="settings_back_helper"), 
-	    InlineKeyboardButton(text="▷", callback_data="help_next")  # Fixed next button
-	]
+	controll_button = [InlineKeyboardButton(text="◁", callback_data=f"settings_back_helper_fixed"), InlineKeyboardButton(text="HOME", callback_data=f"settingsback_helper"), InlineKeyboardButton(text="▷", callback_data=f"settings_back_helper")]
 	second_page_menu = InlineKeyboardMarkup(
 		[
 			[InlineKeyboardButton(text=_["H_B_13"], callback_data="help_callback hb13"), InlineKeyboardButton(text=_["H_B_14"], callback_data="help_callback hb14"), InlineKeyboardButton(text=_["H_B_15"], callback_data="help_callback hb15")],
@@ -39,11 +40,17 @@ def second_page(_):
 		]
 	)
 	return second_page_menu
+	
 
-# Common back button
+# Just an common button
 def help_back_markup(_):
-	return InlineKeyboardMarkup([[InlineKeyboardButton(text=_["BACK_BUTTON"], callback_data="settings_back_helper")]])
+	upl = InlineKeyboardMarkup([[InlineKeyboardButton(text=_["BACK_BUTTON"], callback_data=f"settings_back_helper")]])
+	return upl
 
-# Ease of access
+
+# Ease of access 
 def private_help_panel(_):
-	return [[InlineKeyboardButton(text=_["S_B_4"], url=f"https://t.me/{app.username}?start=help")]]
+	buttons = [[InlineKeyboardButton(text=_["S_B_4"], url=f"https://t.me/{app.username}?start=help")]]
+	return buttons
+
+
