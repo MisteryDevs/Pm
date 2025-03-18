6a7402601fe35934d452e182a489cb127ff855a2
princesinff <166802071+princesinff@users.noreply.github.com>
2025-03-18 21:28:58 +0530

    Update goodnight.py

--- SONALI/plugins/tools/goodnight.py
+++ SONALI/plugins/tools/goodnight.py
@@ -1,24 +1,18 @@
-import re
-from dotenv import load_dotenv
-from pyrogram import filters
 import random
-from pyrogram.types import Message
 from pyrogram import Client, filters
-from SONALI import app
+from pyrogram.types import Message
+from SONALI import app  # Make sure app is initialized correctly
 
-
-
-# "/gn" command ka handler
-@app.on_message(filters.command("oodnight", prefixes="g"))
-def goodnight_command_handler(client: Client, message: Message):
+# Goodnight Message Detector
+@app.on_message(filters.text & filters.regex(r"(?i)\b(good\s?night|gn|goodnight)\b"))
+async def goodnight_auto_reply(client: Client, message: Message):
     # Randomly decide whether to send a sticker or an emoji
     send_sticker = random.choice([True, False])
     
-    # Send a sticker or an emoji based on the random choice
     if send_sticker:
-        client.send_sticker(message.chat.id, get_random_sticker())
+        await client.send_sticker(message.chat.id, get_random_sticker())
     else:
-        client.send_message(message.chat.id, get_random_emoji())
+        await client.send_message(message.chat.id, get_random_emoji())
 
 # Function to get a random sticker
 def get_random_sticker():
@@ -37,6 +31,7 @@ def get_random_emoji():
         "😴",
         "😪", 
         "💤",
-        
+        "🌙",
+        "🛌",
     ]
     return random.choice(emojis)
