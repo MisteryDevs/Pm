1253560b6fd4557df3da0dfaff29316aab120305
princesinff <166802071+princesinff@users.noreply.github.com>
2025-03-18 20:41:27 +0530

    Update love.py

--- SONALI/plugins/tools/love.py
+++ SONALI/plugins/tools/love.py
@@ -1,47 +1,39 @@
 from pyrogram import Client, filters
 import random
 from SONALI import app
-from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 
 def get_random_message(love_percentage):
     if love_percentage <= 30:
         return random.choice([
-            "❍ ʟᴏᴠᴇ ɪs ɪɴ ᴛʜᴇ ᴀɪʀ ʙᴜᴛ ɴᴇᴇᴅs ᴀ ʟɪᴛᴛʟᴇ sᴘᴀʀᴋ.",
-            "❍ ᴀ ɢᴏᴏᴅ sᴛᴀʀᴛ ʙᴜᴛ ᴛʜᴇʀᴇ's ʀᴏᴏᴍ ᴛᴏ ɢʀᴏᴡ.",
-            "❍ ɪᴛ's ᴊᴜsᴛ ᴛʜᴇ ʙᴇɢɪɴɴɪɴɢ ᴏғ sᴏᴍᴇᴛʜɪɴɢ ʙᴇᴀᴜᴛɪғᴜʟ."
+            "Love is in the air but needs a little spark.",
+            "A good start but there's room to grow.",
+            "It's just the beginning of something beautiful."
         ])
     elif love_percentage <= 70:
         return random.choice([
-            "❍ ᴀ sᴛʀᴏɴɢ ᴄᴏɴɴᴇᴄᴛɪᴏɴ ɪs ᴛʜᴇʀᴇ. ᴋᴇᴇᴘ ɴᴜʀᴛᴜʀɪɴɢ ɪᴛ.",
-            "❍ ʏᴏᴜ'ʜᴠ ɢᴏᴛ ᴀ ɢᴏᴏᴅ ᴄʜᴀɴᴄᴇ. ᴡᴏʀᴋ ᴏɴ ɪᴛ.",
-            "❍ ʟᴏᴠᴇ ɪs ʙʟᴏssᴏᴍɪɴɢ, ᴋᴇᴇᴘ ɢᴏɪɴɢ."
+            "A strong connection is there. Keep nurturing it.",
+            "You've got a good chance. Work on it.",
+            "Love is blossoming, keep going."
         ])
     else:
         return random.choice([
-            "❍ ᴡᴏᴡ ! ɪᴛ's ᴀ ᴍᴀᴛᴄʜ ᴍᴀᴅᴇ ɪɴ ʜᴇᴀᴠᴇɴ!",
-            "❍ ᴘᴇʀғᴇᴄᴛ ᴍᴀᴛᴄʜ ! ᴄʜᴇʀɪsʜ ᴛʜɪs ʙᴏɴᴅ.",
-            "❍ ᴅᴇsᴛɪɴᴇᴅ ᴛᴏ ʙᴇ ᴛᴏɢᴇᴛʜᴇʀ. ᴄᴏɴɢʀᴀᴛᴜʟᴀᴛɪᴏɴs!"
+            "Wow! It's a match made in heaven!",
+            "Perfect match! Cherish this bond.",
+            "Destined to be together. Congratulations!"
         ])
-
-EVAA = [
-    [
-        InlineKeyboardButton(text="ᴀᴅᴅ ᴍᴇ ʙᴀʙʏ", url="https://t.me/Sweety_music09_BOT?startgroup=true"),
-    ],
-]
-
+        
 @app.on_message(filters.command("love", prefixes="/"))
 def love_command(client, message):
-    args = message.text.split(" ")[1:]  # Command ke baad ke words extract karna
-
-    if len(args) < 2:
-        response = "❍ ᴘʟᴇᴀsᴇ ᴇɴᴛᴇʀ ᴛᴡᴏ ɴᴀᴍᴇs ᴀғᴛᴇʀ /love ᴄᴏᴍᴍᴀɴᴅ.\n\n**Example:** `/love John Alice`"
-    else:
+    command, *args = message.text.split(" ")
+    if len(args) >= 2:
         name1 = args[0].strip()
         name2 = args[1].strip()
         
         love_percentage = random.randint(10, 100)
         love_message = get_random_message(love_percentage)
 
-        response = f"❖ ʜᴇʀᴇ ɪs ʏᴏᴜʀ ʟᴏᴠᴇ ᴘᴇʀᴄᴇɴᴛᴀɢᴇ ⏤͟͟͞͞★ \n\n❅ {name1} ♥️ + {name2} ♥️ = {love_percentage}%\n\n{love_message}"
+        response = f"{name1}💕 + {name2}💕 = {love_percentage}%\n\n{love_message}"
+    else:
+        response = "Please enter two names after /love command."
+    app.send_message(message.chat.id, response)
 
-    client.send_message(message.chat.id, response, reply_markup=InlineKeyboardMarkup(EVAA))
