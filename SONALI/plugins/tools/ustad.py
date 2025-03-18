4a46478e36091d22324aebca248fcf4741d88733
princesinff <166802071+princesinff@users.noreply.github.com>
2025-03-18 21:17:52 +0530

    Update ustad.py

--- SONALI/plugins/tools/ustad.py
+++ SONALI/plugins/tools/ustad.py
@@ -6,7 +6,7 @@ from SONALI import app  # Ensure this is correctly imported
 @app.on_message(filters.command("owner"))
 async def owner_info(client: Client, message: Message):
     # Group ke liye alag image, private chat ke liye alag image
-    photo_url = "https://i.ibb.co/39WSm9zM/IMG-20250207-080405-192.jpg" if message.chat.type == "private" else "https://te.legra.ph/file/5bf629d10afd4af953585.jpg"
+    photo_url = "https://i.ibb.co/39WSm9zM/IMG-20250207-080405-192.jpg" if message.chat.type == "private" else "https://i.ibb.co/39WSm9zM/IMG-20250207-080405-192.jpg"
 
     # Owner link (jitna bhi required ho, ek hi button me rakh diya hai)
     owner_url = "https://t.me/PRINCE_WEBZ"
